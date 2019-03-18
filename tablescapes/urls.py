622974e9f9915a10999d65311dde3ee3c2f7441e
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-18 14:29:38 -0400

    successfully updated database from sqlite to postgres

--- tablescapes/urls.py
+++ tablescapes/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/core/', permanent=True)),
+    # path('', views.index)
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
