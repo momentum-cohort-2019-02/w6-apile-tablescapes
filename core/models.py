df179323e99e43ec1dbdabc1671084ce4b27a84e
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-19 13:27:18 -0400

    created Post class

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    """Model representing intracacies of a post"""
+    title = models.CharField(max_length=200, null=True, blank=True)
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    date_added = models.DateField('Date Added', auto_now_add=True, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    # Metadata
+    class Meta: 
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        self.set_slug()
+        super().save(*args, **kwargs)    
+
+    def set_slug(self):
+        # If the slug is already set, stop here.
+        if self.slug:
+            return
+
+        base_slug = slugify(self.title)
+        slug = base_slug
+        n = 0
+
+        # while we can find a record already in the DB with the slug
+        # we're trying to use
+        while Post.objects.filter(slug=slug).count():
+            n += 1
+            slug = base_slug + "-" + str(n)
+
+        self.slug = slug       
+
+    
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#####You will need to add code for slugs
