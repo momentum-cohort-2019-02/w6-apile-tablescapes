ff2c8b2391e9f63253b88186b97405947604acfc
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-20 14:51:21 -0400

    worked on likes button

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from core.models import Post, Favorite
 from django.views import generic
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def index(request):
     """View function for the homepage of the site."""
@@ -16,10 +19,17 @@ def post_detail_view(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, "core/post_detail.html", {'post': post})
 
+@require_http_methods(['POST'])
+@login_required
 def post_fav_view(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    fav, created = request.favorite.favorite_fav_set.get_or_create(fav=fav)
+    favorite, created = request.user.favorite_set.get_or_create(post=post)
 
-    return redirect(index())
+    if created:
+        messages.info(request, f"You have favorited {post.title}.")
+    else:
+        messages.info(request, f"You have unfavorited {post.title}.")
+        favorite.delete()
 
+    return redirect('/core/')
